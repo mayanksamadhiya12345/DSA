53d8507efe6e3288d9b506aa52b233e5eb0e17b1
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-11-25 10:51:35 +0530

    Time: 7 ms (96.91%), Space: 13.9 MB (91.13%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void dfs(int i,int j,int n,int m,int initial_color,int color,vector<vector<bool>>& vis,vector<vector<int>>& image)
+    void dfs(int i,int j,int n,int m,int initial_color,int color,vector<vector<int>>& image)
     {
-        if(i<0 || i>=n || j<0 || j>=m || image[i][j]!=initial_color || vis[i][j])
+        if(i<0 || i>=n || j<0 || j>=m || image[i][j]!=initial_color || color==image[i][j])
             return;
         
-        vis[i][j] = true;
-        image[i][j] = color;
-        dfs(i+1,j,n,m,initial_color,color,vis,image);
-        dfs(i-1,j,n,m,initial_color,color,vis,image);
-        dfs(i,j-1,n,m,initial_color,color,vis,image);
-        dfs(i,j+1,n,m,initial_color,color,vis,image);
+        image[i][j] = color;           // assign new color for curr cell
+         
+        dfs(i+1,j,n,m,initial_color,color,image);   // bottom
+        dfs(i-1,j,n,m,initial_color,color,image);   // top
+        dfs(i,j-1,n,m,initial_color,color,image);   // left
+        dfs(i,j+1,n,m,initial_color,color,image);   // right
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
     {
         int n = image.size();
         int m = image[0].size();
         int initial_color = image[sr][sc];
-        vector<vector<bool>> vis(n,vector<bool> (m,false));
         
-        dfs(sr,sc,n,m,initial_color,color,vis,image);
+        dfs(sr,sc,n,m,initial_color,color,image);
         return image;
     }
 };
