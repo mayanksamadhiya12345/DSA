5d4da3b009f552416f6c64228b3a580df112b676
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-11-30 12:58:46 +0530

    Time: 7 ms (49.07%), Space: 8.3 MB (6.37%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -2,37 +2,23 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) 
     {
-        sort(arr.begin(),arr.end());                    // sort the array for cheking same elements
-        int cnt = 1;                                    // for counting the occurance of any element
-        set<int> st;                                    // for storing the occurances
+        map<int,int> mp;                // map for storing each value occurance
         
-        // if arr size is 2 and they are not same , it means they are having same occurance so return return false
-        if(arr.size()==2)
+        for(auto it : arr)
         {
-            return (arr[0]==arr[1]);
+            mp[it]++;                   // storing the occurance
         }
         
-        // start iterating over the arr
-        for(int i=1;i<arr.size();i++)
+        set<int> st;
+        
+        for(auto it :  mp)
         {
-            // if current and prev values are not same then check the occurance of current cnt in the set , if it is already present in the set then return false 
-            if(arr[i-1] != arr[i])
-            {
-                if(st.count(cnt) != 0)
-                {
-                    return false;
-                }
-                else
-                {
-                    st.insert(cnt);                         // if cnt is not in teh set then put it into the set and make cnt 1 again for current value
-                    cnt = 1;
-                }  
-            }
-            else
-            {
-                cnt++;                                      // when curr and prev values are same so increase the count
-            }
+            // if occurance for any value is already present in the set then return false (not unique)
+            if(st.count(it.second)) return false;
+            
+            st.insert(it.second);                    // puting into the set (occurance)
         }
-        return true;
+        
+        return true;                                 // return true at the last when there is no duplicate occurance
     }
 };
