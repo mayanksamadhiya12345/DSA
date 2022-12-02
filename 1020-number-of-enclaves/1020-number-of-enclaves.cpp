05187efd4dc42407ead5e9e465de8de1dae577cf
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-12-02 20:16:46 +0530

    Time: 179 ms (34.18%), Space: 29.3 MB (63.70%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void dfs(int r,int c,int n,int m,vector<vector<int>>& grid)
+    {
+        if(r<0 || c<0 || r>=n || c>=m || grid[r][c]!=1)
+            return;
+        
+        grid[r][c] = 2;                 // start marking boundary conneced 1 so we will not count them for not move
+         
+        dfs(r+1,c,n,m,grid);
+        dfs(r-1,c,n,m,grid);
+        dfs(r,c+1,n,m,grid);
+        dfs(r,c-1,n,m,grid);
+    }
+    
+    int numEnclaves(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                // for all boundary values if there is 1 then cover all its connected one (because from there we can move outside)
+                if(i==0 || j==0 || i==n-1 || j==m-1)
+                {
+                    if(grid[i][j]==1)
+                    {
+                        dfs(i,j,n,m,grid);
+                    }
+                }
+            }
+        }
+        
+        int not_move = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                // count 1 by which we can not move outside (non boundary connected)
+                if(grid[i][j]==1)
+                {
+                    not_move++;
+                }
+            }
+        }
+        
+        return not_move;
+    }
+};
