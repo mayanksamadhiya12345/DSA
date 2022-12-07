80bd3ddb032a46062cbc3753a95f2e5ee19cd1cd
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-12-07 14:26:10 +0530

    Added solution - LeetHub

---
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	bool isPossible(int n, vector<pair<int, int> >& prerequisites) 
+	{
+	    vector<int> adj[n];
+
+        for(auto it : prerequisites)
+        {
+            adj[it.second].push_back(it.first);
+        }
+
+        // use kanh's algo to find topo sort for calculated graph
+        vector<int> indegree(n,0);
+        queue<int> q;
+        vector<int> topo;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(auto it : adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it : adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+
+        // if topo size equal to the number of nodes it means all the nodes are present in topo sort so return true
+        if(topo.size()==n) return true;
+        return false;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+    	int N, P;
+        vector<pair<int, int> > prerequisites;
+        cin >> N;
+        cin >> P;
+        for (int i = 0; i < P; ++i) {
+            int x, y;
+            cin >> x >> y;
+            prerequisites.push_back(make_pair(x, y));
+        }
+        // string s;
+        // cin>>s;
+        Solution ob;
+        if (ob.isPossible(N, prerequisites))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
