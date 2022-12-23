9950cedafe9e1be7a8569846d07fc49c83cc4a9c
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2022-12-23 11:13:30 +0530

    Time: 11 ms (39.16%), Space: 11.9 MB (35.43%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int find(int idx,int buy,vector<int>& prices,vector<vector<int>>& dp)
-    {
-        if(idx>=prices.size()) return 0;                       // we are using idx+2 that's why >=
-        
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        
-        if(buy==1)
-        {
-            return dp[idx][buy] = max(-prices[idx]+find(idx+1,0,prices,dp),0+find(idx+1,1,prices,dp));
-        }
-        else
-        {
-                // idx+2 because we can not buy just after selling
-            return dp[idx][buy] = max(prices[idx]+find(idx+2,1,prices,dp),0+find(idx+1,0,prices,dp));
-        }
-    }
-    
     int maxProfit(vector<int>& prices) 
     {
         int n  = prices.size();
-        vector<vector<int>> dp(n,vector<int> (2,-1));
-        return find(0,1,prices,dp);
+        vector<vector<int>> dp(n+2,vector<int> (2,0));
+        
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                if(buy==1) dp[idx][buy] = max(-prices[idx]+dp[idx+1][0],dp[idx+1][1]);
+                else dp[idx][buy] = max(prices[idx]+dp[idx+2][1],dp[idx+1][0]);
+            }
+        }
+        return dp[0][1];
     }
 };
