ac240b65af42ecbd36c18bbf8c2d58486af171f6
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2023-01-03 13:27:51 +0530

    Time: 19 ms (42.96%), Space: 9 MB (80.04%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
@@ -1,4 +1,3 @@
-// s = "165462"
 class Solution {
 public:
     int minimumPartition(string s, int k) 
@@ -8,15 +7,16 @@ public:
         for(int i=0;i<s.length();i++)
         {
             int digit = s[i]-'0';
-            if(digit > k) return -1;
+            if(digit > k) return -1;                   // if any time our single digit goes bigger then k return -1, there will no partition possible
             
-            num = num*10 + digit;
+            num = num*10 + digit;                      // each time compute num
             
+            // if our computed num goes bigger then count it as a partition
             if(num>k)
             {
                 cnt++;
-                num = s[i]-'0';
-            }
+                num = digit;                        // assign curr digit to num for next calls
+             }
             
         }
         
