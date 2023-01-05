ed516a02832c23bdc73328fc178b80515cf6adad
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2023-01-05 11:59:31 +0530

    Time: 1072 ms (26.20%), Space: 89.7 MB (68.04%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& nums) 
+    {
+        // this questions is just like merge intervels
+        sort(nums.begin(),nums.end());
+        int last_point = nums[0][1];
+        int count = 1;
+        
+        for(auto &it : nums)
+        {
+            // no overlaping
+            if(it[0] > last_point)
+            {
+                // burst it
+                // cout<<it[0]<<" "<<it[1]<<endl;
+                count++;
+                last_point=it[1];             // updated last point
+            }
+            last_point = min(last_point,it[1]);
+        }
+        
+        return count;
+    }
+};
