fbb423b7ee2cae416f7c5b941867bf5aac0b7328
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2023-01-26 11:31:13 +0530

    Time: 36 ms (53.58%), Space: 14.7 MB (23.57%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int solve(int src,int dst,int k,int n,vector<vector<int>> adj[])
     {
-        queue<vector<int>> q;
-        q.push({src,0,0});
-        vector<int> dis(n,INT_MAX);
-        dis[src]=0;
+        queue<vector<int>> q;                       // make queue of vector for storing {node,dist,stop}
+        q.push({src,0,0});                          // {node,dist,stop}
+        vector<int> dis(n,INT_MAX);                 // for keep traking of dist for every node
+        dis[src]=0;                                 // src to src will be 0
         
+        // BFS
         while(!q.empty())
         {
             int node = q.front()[0];
@@ -14,22 +15,25 @@ public:
             int stop = q.front()[2];
             q.pop();
             
-            if(stop>k) continue;
+            if(stop>k) continue;                     // if stop is bigger than k then just continue no need to find cost
             
+            // check adjacency nodes
             for(auto &it : adj[node])
             {
-                int nexNode = it[0];
-                int nexWeight = it[1];
+                int nexNode = it[0];                 // nextNode
+                int nexWeight = it[1];               // nextNode Weight
                 
+                // if i am having better option then took that and push it into queue with stop+1, as i used node as a stop
                 if(cost +  nexWeight < dis[nexNode])
                 {
-                    dis[nexNode] = cost + nexWeight;
+                    dis[nexNode] = cost + nexWeight;                     // taking min cost for nexNode
                     q.push({nexNode, dis[nexNode],stop+1});
                 }
             }
         }
-        if(dis[dst]==INT_MAX) return -1;
-        else return dis[dst];
+        
+        if(dis[dst]==INT_MAX) return -1;                // if I didn't reached to my dst then return -1
+        else return dis[dst];                           // return min cost for dst node from  dis
     }
     
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
@@ -41,6 +45,7 @@ public:
             adj[it[0]].push_back({it[1],it[2]});
         }
         
+        // Function calling for BFS
         int ans = solve(src,dst,k,n,adj);
         return ans;
     }
