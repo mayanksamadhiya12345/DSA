ac0ec27a8e4c390c811e5ce1ed7ea054482a512c
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2024-07-14 21:48:55 +0530

    Time: 25 ms (73.02%), Space: 8.2 MB (85.81%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -4,18 +4,19 @@ public:
     {
         int n = s1.length();
         int m = s2.length();
-        vector<vector<int>> dp(n+1, vector<int> (m+1,0));
+        vector<int> curr(m+1,0), prev(m+1,0);
 
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                if(s1[i-1]==s2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                if(s1[i-1]==s2[j-1]) curr[j] = 1+prev[j-1];
+                else curr[j] = max(prev[j], curr[j-1]);
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
     int lps(string &s)
     {
