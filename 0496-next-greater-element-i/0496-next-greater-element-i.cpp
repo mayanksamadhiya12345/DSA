7434ba8c1360de65d2c4f702c1a39bcf41a388b7
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2024-08-11 22:46:51 +0530

    Time: 8 ms (31.06%), Space: 12.1 MB (44.71%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> mp;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        for(int i=0;i<n2;i++)
+        {
+            mp[nums2[i]] = i;
+        }
+
+        vector<int> ans;
+        for(int i=0;i<n1;i++)
+        {
+            int curr = nums1[i];
+            int idx = mp[curr];
+            bool flag = false;
+
+            for(int j=idx+1;j<n2;j++)
+            {
+                // If I'm findign any big value then that will be the part of my answer vector
+                if(nums2[j] > curr)
+                {
+                    ans.push_back(nums2[j]);
+                    // after taking the big value there is no mean to procedd further so just break the loop by saying I found one big value
+                    flag = true;
+                    break;
+                }
+            }
+            // if we have not found any big value in that case -1 should be included
+            if(flag == false) ans.push_back(-1);
+        }
+        return ans;
+    }
+};
+
+// 
