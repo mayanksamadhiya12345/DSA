37bd9b79a0a4f3e9375971d3dc985d1a19867e24
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2024-09-01 13:45:09 +0530

    Time: 85 ms (44.77%), Space: 91.5 MB (48.92%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -3,14 +3,17 @@ public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         int s = original.size();
         if(m*n != s) return {};
-        vector<vector<int>> nums(m, vector<int>(n));
-        int k = 0;
 
-        for(int i=0;i<m;i++)
+        vector<vector<int>> nums;
+
+        vector<int> temp;
+        for(int i=0;i<s;i++)
         {
-            for(int j=0;j<n;j++)
+            temp.push_back(original[i]);
+            if((i+1)%n == 0)
             {
-                nums[i][j] = original[k++];
+                nums.push_back(temp);
+                temp.erase(temp.begin(), temp.end());
             }
         }
         return nums;
