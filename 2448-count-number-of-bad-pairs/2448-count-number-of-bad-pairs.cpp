c267110b0d4f0667b8a934524c2abff6bb8e6ce8
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2025-02-09 12:26:07 +0530

    Time: 156 ms (5.01%), Space: 89.4 MB (35.26%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -5,21 +5,26 @@ public:
         long long n = nums.size();
         long long total = (long long) n*(n-1)/2;
         map<long long,long long> mp;
+        long long good = 0;
         
         for(int i=0;i<n;i++)
         {
             mp[nums[i]-i]++;
-        }
 
-        long long good = 0;
-        for(auto&it : mp)
-        {
-            int curr = it.second;
-            if(curr > 1)
+            if(mp[nums[i]-i] > 1)
             {
-                good += (long long) curr*(curr-1)/2;
+                good += (mp[nums[i]-i]-1);
             }
         }
+        // for(auto&it : mp)
+        // {
+        //     int curr = it.second;
+        //     if(curr > 1)
+        //     {
+        //         good += (long long) curr*(curr-1)/2;
+        //     }
+        // }
+        cout<<good;
         return total-good;
     }
 };
