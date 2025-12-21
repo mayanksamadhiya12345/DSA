c9ba9e41d04a1741b1fc6096351a6ae827d1a8be
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2025-12-21 10:53:41 +0530

    Time: 16 ms (5.8%), Space: 24.7 MB (34.99%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) 
-    {
-        map<int,set<int>> rows;
-        map<int,set<int>> cols;
-        map<pair<int,int>,set<int>> subBox;
-        
-        for(int r=0;r<9;r++)
-        {
-            for(int c=0;c<9;c++)
-            {
-                // if it is . skip it
-                if(board[r][c]=='.') continue;
-                
-                // if we found current value previously then return false
-                
-                // Check row
-                if(rows[r].find(board[r][c]-'0') != rows[r].end())
-                    return false;
-                
-                // check col
-                if(cols[c].find(board[r][c]-'0') != cols[c].end())
-                    return false;
-                
-                // check sub boxes
-                if(subBox[{r/3,c/3}].find(board[r][c]-'0') != subBox[{r/3,c/3}].end())
-                    return false;
-                
-                
-                // if none of then return false then add curr board value to the declared set for next check
-                rows[r].insert(board[r][c]-'0');
-                cols[c].insert(board[r][c]-'0');
-                subBox[{r/3,c/3}].insert(board[r][c]-'0');
+    bool isValidSudoku(vector<vector<char>>& board) {
+        map<int, set<int>> rows, cols;
+        map<pair<int, int>, set<int>> subBox;
+        for(int i=0;i<9;i++) {
+            for(int j=0;j<9;j++) {
+                if(board[i][j] == '.') continue;
+
+                if(rows[i].find(board[i][j]-'0') != rows[i].end()) return false;
+                if(cols[j].find(board[i][j]-'0') != cols[j].end()) return false;
+                if(subBox[{i/3,j/3}].find(board[i][j]-'0') != subBox[{i/3,j/3}].end()) return false;
+
+                rows[i].insert(board[i][j]-'0');
+                cols[j].insert(board[i][j]-'0');
+                subBox[{i/3,j/3}].insert(board[i][j]-'0');
             }
         }
-        
         return true;
     }
 };
