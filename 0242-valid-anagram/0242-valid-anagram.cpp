ccad0f8630ced6b86752c3e056494fc637e852e9
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2025-12-21 09:42:11 +0530

    Time: 11 ms (7.6%), Space: 9.9 MB (10.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,8 +2,24 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         // Approach 1
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return s==t;
+
+        // Approach 2
+        map<int,int> mp1, mp2;
+        for(auto&it : s) mp1[it]++;
+        for(auto&it : t) mp2[it]++;
+
+        for(auto&it : t) {
+            if(mp1[it] == 0) return false;
+            mp1[it]--;
+        }
+
+        for(auto&it : s) {
+            if(mp2[it] == 0) return false;
+            mp2[it]--;
+        }
+        return true;
     }
 };
