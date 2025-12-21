f19a32c640f6241a159ee9f888bd3eea848c9d85
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2025-12-21 09:44:35 +0530

    Time: 4 ms (53.53%), Space: 15 MB (13.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) 
-    {
-        // 3 -> 0, 2->1, 4->2
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        int n = nums.size();
+        for(int i=0;i<n;i++) {
+            int a = nums[i];
+            int b = target-a;
 
-        // 
-        int n = numbers.size();
-        vector<pair<int,int>> aashi;
-        for(int i=0;i<n;i++)
-        {
-            aashi.push_back({numbers[i], i});
+            if(mp.find(b) != mp.end()) {
+                return {i, mp[b]};
+            }
+            mp[a] = i;
         }
-
-        sort(aashi.begin(),aashi.end());
-        // 2->1, 3->0, 4->2
-
-        int st = 0;
-        int en = n-1;
-
-        while(st < en)
-        {
-            cout<<aashi[st].first<<" "<<aashi[en].first<<endl;
-            if(aashi[st].first + aashi[en].first == target) return {aashi[st].second, aashi[en].second};
-            else if(aashi[st].first + aashi[en].first > target) en--;
-            else st++;
-        }
-        return {-1,-1};
-
+        return {-1, -1};
     }
 };
