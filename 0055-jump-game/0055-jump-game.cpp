2b4ce0ebd09ce90eecbe3d31404c43cd5043b37f
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2026-05-17 22:58:06 +0530

    Time: 0 ms (100%), Space: 52.4 MB (23.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) 
-    {
+    bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int reachable = 0;
-        for(int i=0;i<n;i++)
-        {
-            if(i>reachable) return false;                           // if my max jump is lower than i it means i can not reach there
-            else reachable = max(reachable,i+nums[i]);              // how max i can jump on a particular index
+        int mxJump = 0;
+        for(int i=0;i<n;i++) {
+            if(i>mxJump) return false;
+            else mxJump = max(mxJump, nums[i]+i);
         }
         return true;
     }
