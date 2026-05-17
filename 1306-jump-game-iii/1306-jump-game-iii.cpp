4894b2c629d8400fc3111886a8f1d97a44a7616f
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2026-05-18 00:04:38 +0530

    Time: 7 ms (17.61%), Space: 42.1 MB (10.85%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    bool solve(vector<int>& arr, int n, int start, vector<bool>& vis) {
+    bool solve(vector<int>& arr, int n, int start, vector<bool>& vis, vector<int>& dp) {
         if(start<0 || start>=n || vis[start])
             return false;
+
+        if(dp[start]!=-1) return dp[start];
 
         vis[start] = true;
         if(arr[start]==0) return true;
 
-        return solve(arr, n, start-arr[start], vis) || solve(arr, n, start+arr[start], vis);
+        return dp[start] = solve(arr, n, start-arr[start], vis, dp) || solve(arr, n, start+arr[start], vis, dp);
     }
     bool canReach(vector<int>& arr, int start) {
         int n=arr.size();
         vector<bool> vis(n, false);
-        return solve(arr, n, start, vis);
+        vector<int> dp(n, -1);
+        return solve(arr, n, start, vis, dp);
     }
 };
