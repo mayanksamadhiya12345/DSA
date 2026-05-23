4e4e610cfc40b507876422c74f59d1eabb8cb2fc
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2026-05-23 13:58:44 +0530

    Time: 0 ms (100%), Space: 17.5 MB (60.6%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     bool checkBip(int node, vector<int>& color, vector<vector<int>>& graph) {
-        if(color[node]==-1) color[node]=0;
-        for(auto&it : graph[node]) {
-            if(color[it] == -1) { 
-                color[it] = 1-color[node];
-                if(!checkBip(it, color, graph)) {
+        color[node] = 0;
+        queue<int> q;
+        q.push(node);
+
+        while(!q.empty()) {
+            int Node = q.front();
+            q.pop();
+
+            for(auto&it : graph[Node]) {
+                if(color[it] == -1) {
+                    color[it] = 1-color[Node];
+                    q.push(it);
+                }
+                else if(color[it] == color[Node]) {
                     return false;
                 }
-            }
-            else if (color[it] == color[node]){
-                return false;
             }
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-
         vector<int> color(n, -1);
         for(int i=0;i<n;i++) {
-            if(color[i]==-1) {
-                if(!checkBip(i, color, graph)) {
-                    return false;
-                }
+            if(color[i] == -1) {
+                if(!checkBip(i, color, graph)) return false;
             }
         }
         return true;
