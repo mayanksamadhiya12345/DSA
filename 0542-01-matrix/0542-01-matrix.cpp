f625672285567a22e8d70bb59072b7cab73fc9b1
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2026-05-27 14:46:25 +0530

    Time: 30 ms (21.49%), Space: 36.2 MB (53.01%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,51 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) 
-    {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        queue<pair<pair<int,int>,int>> q;
-        vector<vector<bool>> vis(n,vector<bool> (m,false));
-        
-        
-        // pushing all cells that are having value 1 with step 0
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]==0 && !vis[i][j])
-                {
-                    q.push({{i,j},0});                              // position,steps
-                    vis[i][j] = true;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size(), m=mat[0].size();
+        queue<pair<pair<int, int>, int>> q;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(mat[i][j]==0) {
+                    q.push({{i, j}, 0});
+                    mat[i][j]=2;
                 }
             }
         }
-        
-        
-        vector<vector<int>> ans(n,vector<int> (m,0));
-        while(!q.empty())
-        {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
+
+        vector<vector<int>> ans(n, vector<int> (m, 0));
+        while(!q.empty()) {
+            int r = q.front().first.first;
+            int c = q.front().first.second;
             int step = q.front().second;
             q.pop();
-            
-            ans[row][col] = step;                              // put the step count for current row col cell
-            
-            // possible 4 direction
-            int delrow[] = {-1,0,+1,0};
-            int delcol[] = {0,-1,0,+1};
-            
-            for(int i=0;i<4;i++)
-            {
-                int nrow = row+delrow[i];
-                int ncol = col+delcol[i];
-                
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol])
-                {
-                    vis[nrow][ncol] = true;
-                    q.push({{nrow,ncol},step+1});
+
+            ans[r][c] = step;
+
+            int delr[4] = {-1, 0, 1, 0};
+            int delc[4] = {0, -1, 0, 1};
+            for(int i=0;i<4;i++) {
+                int nr = r+delr[i];
+                int nc = c+delc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && mat[nr][nc]!=2) {
+                    q.push({{nr, nc}, step+1});
+                    mat[nr][nc]=2;
                 }
             }
         }
