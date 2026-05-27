e5f7590bc6632fc0dfa655553244657c3d49eb11
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2026-05-27 14:37:28 +0530

    Time: 0 ms (100%), Space: 18.2 MB (43.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void solve(int i,int j,int n,int m,int init_color,int color,vector<vector<int>>& image)
-    {
-        if(i<0 || j<0 || i>=n || j>=m || color==image[i][j] || init_color!=image[i][j]) return;
+    void solve(int i, int j, int n, int m, int init_color, int color, vector<vector<bool>>& vis, vector<vector<int>>& image) {
+        if(i<0 || i>=n || j<0 || j>=m || image[i][j]==color || vis[i][j] || image[i][j]!=init_color) {
+            return;
+        }
 
+        vis[i][j] = true;
         image[i][j] = color;
-
-        solve(i-1,j,n,m,init_color,color,image);
-        solve(i+1,j,n,m,init_color,color,image);
-        solve(i,j-1,n,m,init_color,color,image);
-        solve(i,j+1,n,m,init_color,color,image);
+        solve(i+1, j, n, m, init_color, color, vis, image);
+        solve(i-1, j, n, m, init_color, color, vis, image);
+        solve(i, j-1, n, m, init_color, color, vis, image);
+        solve(i, j+1, n, m, init_color, color, vis, image);
+        return;
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-
+        int n=image.size(), m=image[0].size();
         int init_color = image[sr][sc];
-        solve(sr, sc, n, m, init_color, color, image);
+        vector<vector<bool>> vis(n, vector<bool> (m, false));
+
+        solve(sr, sc, n, m, init_color, color, vis, image);
         return image;
     }
 };
