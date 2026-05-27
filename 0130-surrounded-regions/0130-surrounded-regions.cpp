965af61a9aa7c7741ff0c2369857a5dfdfe64a99
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2026-05-27 14:51:59 +0530

    Time: 11 ms (5.02%), Space: 14.3 MB (69.9%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,50 +1,35 @@
 class Solution {
 public:
-    void dfs(int i,int j,int n,int m,vector<vector<bool>>& vis,vector<vector<char>>& board)
-    {
-        if(i<0 or i>=n or j<0 or j>=m or board[i][j]!='O' or vis[i][j]==true)
+    void solve(int i, int j, int n, int m, vector<vector<bool>>& vis, vector<vector<char>>& board) {
+        if(i<0 || i>=n || j<0 || j>=m || vis[i][j] || board[i][j] != 'O')
             return;
-        
-        vis[i][j]=true;
-        
-        dfs(i+1,j,n,m,vis,board);
-        dfs(i-1,j,n,m,vis,board);
-        dfs(i,j+1,n,m,vis,board);
-        dfs(i,j-1,n,m,vis,board);
+
+        vis[i][j] = true;
+        solve(i+1, j, n, m, vis, board);
+        solve(i-1, j, n, m, vis, board);
+        solve(i, j+1, n, m, vis, board);
+        solve(i, j-1, n, m, vis, board);
+        return;
     }
-    
-    void solve(vector<vector<char>>& board) 
-    {
+    void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
-        
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                // if it is boundary element
-                if(i==0 or j==0 or i==n-1 or j==m-1)
-                {
-                    if(vis[i][j]==false && board[i][j]=='O')
-                    {
-                        dfs(i,j,n,m,vis,board);
-                    }
+        vector<vector<bool>> vis(n, vector<bool> (m, false));
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(i==0 || i==n-1 || j==0 || j==m-1) {
+                    solve(i, j, n, m, vis, board);
                 }
             }
         }
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(vis[i][j]==true)
-                {
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(vis[i][j] == true) {
                     board[i][j] = 'O';
                 }
-                else
-                {
+                else {
                     board[i][j] = 'X';
                 }
             }
