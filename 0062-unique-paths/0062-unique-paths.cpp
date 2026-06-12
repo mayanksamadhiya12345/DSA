936e8c9da674b5008be109a8bdde442c51978cd3
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2026-06-12 23:47:54 +0530

    Time: 0 ms (100%), Space: 9.5 MB (28.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,7 +9,23 @@ public:
         return dp[i][j] = left+up;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return solve(m-1,n-1, dp);
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        dp[0][0] = 1;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(i==0 && j==0)
+                    continue;
+                int left=0, up=0;
+
+                if(j>0)
+                    left = dp[i][j-1];
+
+                if(i>0)
+                    up = dp[i-1][j];
+
+                dp[i][j] = left+up;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
