b8653efb5de3da67e7c6a73c7c1724b9a4d4a3db
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2026-06-21 16:27:58 +0530

    Time: 1443 ms (5.38%), Space: 105.1 MB (5.01%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int maxIceCream(vector<int>& costs, int coins) 
-    {
-        sort(costs.begin(),costs.end());
-        if(costs[0] > coins) return 0;
-        
-        int count=0;
-        int i=0;
-        while(coins && i<costs.size())
-        {
-            if(costs[i] <= coins)
-            {
-                count++;
-                coins -= costs[i++];
-            }
-            else
-            {
-                break;
-            }
+    int maxIceCream(vector<int>& costs, int coins) {
+        map<int, int> mp;
+
+        for(auto&it : costs) {
+            mp[it]++;
         }
-        return count;
+
+        int ans = 0;
+        for(auto&it : mp) {
+            int curr_price = it.first;
+            int no_of_times = it.second;
+            while(coins >= curr_price && no_of_times) {
+                ans++;
+                coins -= curr_price;
+                no_of_times--;
+            }
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+
+        return ans;
     }
 };
