da1fda68673981871c9e18170a13956a60952fc0
Mayank Samadhiya <75272692+mayanksamadhiya12345@users.noreply.github.com>
2026-08-02 14:37:04 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.75%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        int suma = 0;
+        int sumb = 0;
+        for(int i=0; i<n/2; i++){
+            suma += piles[i];
+            sumb += piles[n-i-1];
+        }
+
+        int alice =0;
+        int bob = 0;
+        bool flag = true;
+        int i = 0;
+        int j = n-1;
+        while(i<j){
+            if(flag){
+                if(piles[i] == piles[j]){
+                    alice += piles[i];
+                    if(suma >= sumb){
+                        j--;
+                    }
+                    else{
+                        i++;
+                    }
+                }
+                else if(piles[i] > piles[j]){
+                    alice += piles[i];
+                    i++;
+                }
+                else{
+                    alice += piles[j];
+                    j--;
+                }
+            }
+            else{
+                if(piles[i] == piles[j]){
+                    bob += piles[i];
+                    if(suma >= sumb){
+                        j--;
+                    }
+                    else{
+                        i++;
+                    }
+                }
+                else if(piles[i] > piles[j]){
+                    bob += piles[i];
+                    i++;
+                }
+                else{
+                    bob += piles[j];
+                    j--;
+                }
+            }
+        }
+
+        return alice >= bob;
+    }
+};
